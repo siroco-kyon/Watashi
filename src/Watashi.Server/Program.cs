686595f24d5bbeb6598d204dff438dc50ccff8e3
siroco-kyon <ku.emile.0519@gmail.com>
2026-06-01 22:00:03 +0900

    fix: raise request body limit to 10GB

--- src/Watashi.Server/Program.cs
+++ src/Watashi.Server/Program.cs
@@ -20,7 +20,7 @@ using Watashi.Shared.Constants;
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseWindowsService(o => o.ServiceName = "Watashi.Server");
 
-const long DefaultMaxRequestBodySize = 1L * 1024 * 1024 * 1024;
+const long DefaultMaxRequestBodySize = 10L * 1024 * 1024 * 1024;
 var maxRequestBodySize = builder.Configuration.GetValue<long?>("Kestrel:Limits:MaxRequestBodySize")
     ?? DefaultMaxRequestBodySize;
 builder.WebHost.ConfigureKestrel(o => o.Limits.MaxRequestBodySize = maxRequestBodySize);
