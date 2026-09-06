8ecb85f2b186bdc83f3f2911e7fd1c43f6b0d39f
siroco-kyon <ku.emile.0519@gmail.com>
2026-09-06 13:26:57 +0900

    Add accessible About window with version copy and torii greetings

--- src/Watashi.Client/Views/MainWindow.xaml.cs
+++ src/Watashi.Client/Views/MainWindow.xaml.cs
@@ -169,9 +169,7 @@ public partial class MainWindow : Window
 
     private void OnAbout(object sender, RoutedEventArgs e)
     {
-        var version = AppVersion.Display;
-        MessageBox.Show($"Watashi - 社内 CIFS ファイル管理ツール\nバージョン {version}\n\n⛩ 鳥居をくぐって、信頼できる場所へ。",
-                        "バージョン情報", MessageBoxButton.OK, MessageBoxImage.Information);
+        new Views.AboutWindow { Owner = this }.ShowDialog();
     }
 
     private void OnOpenTrustedDevices(object sender, RoutedEventArgs e)
